38e0cc571d84d577fd6706c914c2e654c6001469
Alvin <>
2020-11-30 16:37:50 +0400

    using local references in templates

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,6 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string | undefined, serverContent: string | undefined}>();
   // tslint:disable-next-line:no-output-rename
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName: string | undefined, serverContent: string | undefined}>();
-  newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -18,12 +17,12 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
   }
 
-  onAddBluePrint() {
-    this.bluePrintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddBluePrint(nameInput: HTMLInputElement) {
+    this.bluePrintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
   }
 
 }
