bfa132286095d4c355e9e645f3dc48223b80ffcb
Alvin <>
2020-12-05 15:50:50 +0400

    binding exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,4 +32,19 @@ export class AppComponent {
     this.serverElements.splice(0, 1);
   }
 
+  // tslint:disable-next-line:member-ordering
+  oddNumbers: number[] = [];
+  // tslint:disable-next-line:member-ordering
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+
+
+
 }
